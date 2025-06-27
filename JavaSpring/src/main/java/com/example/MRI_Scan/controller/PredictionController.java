d5a744c4d7f7f19aded36d59dec5dc0afa0ad2dd
Yussuf Ahmed <mtawolfroleplay@gmail.com>
2025-06-27 08:17:18 +0300

    Update PredictionController.java

--- JavaSpring/src/main/java/com/example/MRI_Scan/controller/PredictionController.java
+++ JavaSpring/src/main/java/com/example/MRI_Scan/controller/PredictionController.java
@@ -1,9 +1,7 @@
 package com.example.MRI_Scan.controller;
 
 import java.util.Base64;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.http.*;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.ui.Model;
@@ -13,15 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.File;
+import org.springframework.stereotype.Controller;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-@org.springframework.stereotype.Controller
-public class Controller {
+////////////////////////////////////////////////
+//////////////REMEMBER//////////////////////////
+//TO CHECK THAT THE PHOTO THAT APPEARS
+//WITH PREDICTION ARE FROM THE UPLOAD
+//OF THE USER AND PHOTOS NOT RETURN
+//FROM FAST API TO REDUCE TIME
+//////////////REMEMBER//////////////////////////
+////////////////////////////////////////////////
+@Controller
+public class PredictionController {
     @Autowired
     private RestTemplate restTemplate;
 
@@ -45,7 +46,7 @@ public class Controller {
     ResponseEntity<String> response = restTemplate.postForEntity(fastApiUrl, request, String.class);
     String fileName = file.getOriginalFilename();
     String base64Image = Base64.getEncoder().encodeToString(file.getBytes());
-    String imageSrc = "data:image/jpeg;base64," + base64Image; 
+    String imageSrc = "data:image/jpeg;base64," + base64Image;
 
 redirectAttributes.addFlashAttribute("imagePath", imageSrc);
     String result = response.getBody();
